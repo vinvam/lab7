20dadacc5d9539367fee4b58300f8d3b8070a864
vagrant <vagrant@precise32.(none)>
2014-02-21 02:31:34 +0000

    lab7

--- run_mongo.sh
+++ run_mongo.sh
@@ -6,8 +6,7 @@ sys_cygwin="0"
 sys_osx="0"
 
 system=$(uname -a)
-
-if [ "${system:0:15}" == "Linux precise32" ]
+if [ "$system" == "Linux precise32 3.2.0-23-generic-pae #36-Ubuntu SMP Tue Apr 10 22:19:09 UTC 2012 i686 i686 i386 GNU/Linux" ]
 then
   sys_vagrant="1"  
   echo "Running on Vagrant guest"
